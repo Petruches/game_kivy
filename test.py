1eb7f0e1eea8e5d3e82a23549205d2a8208bff37
p.bykov <p.bykov@qiwi.com>
2024-02-25 19:15:32 +0300

    remove tresh

--- test.py
+++ test.py
@@ -20,8 +20,9 @@ Window.clearcolor = (0, 0, 0, 1)
 
 #ivent = ["Вы нашли\nсундук", "На вас напали\nбандиты", "Вы нашли\nдеревню", "Вы нашли\nдракона", "На вас\nнапали волки"]
 ivent = ["На вас напали\nбандиты", "На вас\nнапали волки"]
-hp = 100
+#hp = 100
 enemy_battle = False
+strng = "Вы погибли"
 
 box1 = BoxLayout(orientation='horizontal', size_hint=(None, 1))
 box2 = BoxLayout(orientation='vertical')
@@ -29,8 +30,8 @@ box3 = BoxLayout(orientation='horizontal')
 gridox2 = GridLayout(cols=2)
 
 def start(self):
-	self.label = Label(text="Вы погибли", halign="center", font_size=30, font_name="press-start-2p-regular", color=(0, 1, 0, 1))
-	self.label1 = Label(text=f"{hp} hp", font_size=30, size_hint=(None, None), size=(350, 100), pos_hint=({'top': 1}), font_name="press-start-2p-regular", color=(0, 1, 0, 1))
+	self.label = Label(text=strng, halign="center", font_size=30, font_name="press-start-2p-regular", color=(0, 1, 0, 1))
+	self.label1 = Label(text=f"{Player.LIFE} hp", font_size=30, size_hint=(None, None), size=(350, 100), pos_hint=({'top': 1}), font_name="press-start-2p-regular", color=(0, 1, 0, 1))
 	self.label2 = Label(text="100 mn", font_size=30, size_hint=(None, None), size=(350, 100), pos_hint=({'top': 1}), font_name="press-start-2p-regular", color=(0, 1, 0, 1))
 	#self.button1 = Button(text="Кнопка", size_hint=(None, None), size=(350, 100), font_name="press-start-2p-regular", background_color=(1, 0, 0, 1), color=(0, 1, 0, 1))
 	#self.button2 = Button(text="Кнопка", size_hint=(None, None), size=(350, 100), font_name="press-start-2p-regular", background_color=(1, 0, 0, 1), color=(0, 1, 0, 1))
@@ -45,6 +46,9 @@ def start(self):
 	self.button13.bind(on_press=self.callback)
 	self.button14.bind(on_press=self.att)
 
+
+class Player():
+	LIFE = 100
 
 class MyApp(App):
 
@@ -87,44 +91,32 @@ class MyApp(App):
 		if (self.label.text == rnd):
 			random.shuffle(ivent)
 			rnd = random.choice(ivent)
-			self.label.text = rnd
+			strng = rnd
+			self.label.text = strng
 		else:
-			self.label.text = rnd
+			strng = rnd
+			self.label.text = strng
 
-		if (self.label.text == "На вас напали\nбандиты"):
+		if (strng == "На вас напали\nбандиты"):
 			self.button11.disabled = True
 			self.button12.disabled = True
-			self.bitva(self)
+			self.bitva()
 			print("F")
 
 
-	async def bitva(self, *args, **kwargs):
-		sss=True
-		while(sss):
-			if (self.label.text == "Вы ударили"):
-				await asyncio.sleep(2)
-				print(f"'{self.label.text}'", self.label.text == "Вы ударили")
-				print("Нанесен удар по нпс")
-				self.button11.disabled = False
-				self.button12.disabled = False
-				sss=False
-				break
-			elif (self.label.text == "Вы промахнулись"):
-				await asyncio.sleep(2)
-				print(self.label.text)
-				await print("Пропуск хода")
-				self.button11.disabled = False
-				self.button12.disabled = False
-				sss=False
-				break
-
 	def att(self, *args, **kwargs):
-		self.label.text = "Вы ударили"
+		strng = "Вы ударили"
+		self.label.text = strng
+		print(strng)
+		#self.label.text = "Вы ударили"
 		print(f"'{self.label.text}'", self.label.text == "Вы ударили")
 
 
 	def callback(self, *args, **kwargs):
-		self.label.text = "гг изи\nхыхы"
+		strng = "гг изи\nхыхы"
+		self.label.text = strng
+		print(strng)
+		#self.label.text = "гг изи\nхыхы"
 		print(self.label.text)
 
 	def tst2(self, *args, **kwargs):
@@ -133,36 +125,6 @@ class MyApp(App):
 		hp=hp-1
 		print(hp)
 		self.label1.text = f"{hp} hp"
-'''
-	def battle(self, *args, **kwargs):
-		self.button11.disabled = True
-		self.button12.disabled = True
-		
-		sss=True
-		global battle
-		global hp
-		enemy_battle = True
-		enemy_nps_hp=100
-		enemy_ivent = ["Удар", "Мимо"]
-		my_ivent = ["Удар", "Мимо"]
-
-		while enemy_nps_hp != 0:
-			random.shuffle(enemy_ivent)
-			enemy_rnd = random.choice(enemy_ivent)
-
-			self.label.text = enemy_rnd
-			if (enemy_rnd == "Удар"):
-				hp=hp-10
-				self.label1.text = f"{hp} hp"
-			elif (enemy_rnd == "Мимо"):
-				self.label.text == "Мимо"
-			print("Должен быть твой ход")
-			#while(sss):
-				#self.button13.on_press
-
-	def but1(self, *args, **kwargs):
-			print("dsa")
-'''
 
 # Запуск проекта
 if __name__ == "__main__":
